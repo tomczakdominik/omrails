1ebb72e134bcc817d1bec76eba6178822b6985a9
Dominik Tomczak <tomczakdominik@gmail.com>
2014-05-29 22:08:13 +0200

    'paperclip'

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -15,7 +15,7 @@ class Pin < ActiveRecord::Base
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-  validates :user_id, presence: true
+  # validates :user_id, presence: true
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
